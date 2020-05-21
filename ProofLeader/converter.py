02d3bed9445292a24ec50e972eecffbbf356871b
xryuseix <ryusei143.shootingstar@gmail.com>
2020-05-22 02:15:55 +0900

    変換されない箇所を<pre>だけでなく```も追加

--- ProofLeader/converter.py
+++ ProofLeader/converter.py
@@ -17,42 +17,41 @@ def word2Word(text, file):
     if not os.path.isfile("./ProofLeader/word_list.csv"):
         return text
     wordList = File.readFile("./ProofLeader/word_list.csv", True)
-    textArr = text.split("```")
-    lines = 0
-    for i, arr in enumerate(textArr):
-        if i % 2:
-            lines += len(arr)
-            continue
-        textList = arr.split("\n")
-        for j, text in enumerate(textList):
-            for k in wordList:
-                reObj = re.search(k[0], text)
-                if reObj:
-                    print(
-                        "\033[33mWARNING\033[0m: {}:{}:{}: ({}) => ({})".format(
-                            file, lines + j + 1, reObj.start(), reObj.group(), k[1]
-                        )
+    textArr = text.splitlines()
+    for i, text in enumerate(textArr):
+        for li in wordList:
+            reObj = re.search(li[0], text)
+            if reObj:
+                print(
+                    "\033[33mWARNING\033[0m: {}:{}:{}: ({}) => ({})".format(
+                        file, i + 1, reObj.start(), reObj.group(), li[1]
                     )
-    return "```".join(textArr)
+                )
+    return "\n".join(textArr)
 
 
 # 数字を三桁ごとに区切ってカンマ&前後に空白を入れる
 def numComma(text):
+    resText = ""
     digit = "(\d)(?=(\d{3})+(?!\d))"
-    textArr = re.split("<pre>|<\/pre>", text)
-    returnStr = ""
-    for i, crrText in enumerate(textArr):
+    delIndex = [m.span() for m in re.finditer("<pre>|</pre>|```", text)]
+    delIndex.insert(0, [0, 0])
+    delIndex.append([len(text), len(text)])
+
+    for i in range(len(delIndex) - 1):
+        subText = text[delIndex[i][1] : delIndex[i + 1][0]]
         if not i % 2:
-            crrText = re.sub("([^\n\d, ])(\d+)", r"\1 \2", crrText)  # 数値の前に空白
-            crrText = re.sub("(\d)([^\n\d, ])", r"\1 \2", crrText)  # 数値の後ろに空白
-            crrText = re.sub("(\n[a-zA-Z]+)[亜-熙ぁ-んァ-ヶ]", r"\1 ", crrText)  # 先頭英字の後ろに空白
-            crrText = re.sub(digit, r"\1,", crrText)
-            if i > 0:
-                crrText = "</pre>" + crrText
+            subText = re.sub("([^\n\d, ])(\d+)", r"\1 \2", subText)  # 数値の前に空白
+            subText = re.sub("(\d)([^\n\d, ])", r"\1 \2", subText)  # 数値の後ろに空白
+            subText = re.sub("(\n[a-zA-Z]+)[亜-熙ぁ-んァ-ヶ]", r"\1 ", subText)  # 先頭英字の後ろに空白
+            subText = re.sub(digit, r"\1,", subText)
+            if i + 1 < len(delIndex):
+                resText += subText + text[delIndex[i + 1][0] : delIndex[i + 1][1]]
+            else:
+                resText += subText
         else:
-            crrText = "<pre>" + crrText
-        returnStr += crrText
-    return returnStr
+            resText += subText + text[delIndex[i + 1][0] : delIndex[i + 1][1]]
+    return resText
 
 
 file = sys.argv[1]
