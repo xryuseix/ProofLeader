1da7dabd5b431d9320b666022a0af4235b48371e
xryuseix <ryusei143.shootingstar@gmail.com>
2020-05-23 08:37:54 +0900

    os.commandをやめた

--- ProofLeader/readme_dfs.py
+++ ProofLeader/readme_dfs.py
@@ -1,29 +1,27 @@
 import pathlib
-import sys
 import os
 import read_file as File
-
-exFiles = File.readFile("./ProofLeader/exclusion_list.csv").split(',\n')
-
-files = []
+import converter
 
 
-def recursive(path):  # dfs
-    for po in path.iterdir():
-        if po.is_dir():
-            recursive(po)
-        elif po.is_file() & po.match("*.md"):
-            if not str(po) in exFiles:
-                files.append(str(po))
+def dfs(dir=""):
+    exFiles = File.readFile("./ProofLeader/exclusion_list.csv").split(",\n")
 
+    files = []
 
-root = "./"
-if len(sys.argv) == 2:
-    root += sys.argv[1]
-recursive(pathlib.Path(root))
+    def recursive(path):  # dfs
+        for po in path.iterdir():
+            if po.is_dir():
+                recursive(po)
+            elif po.is_file() & po.match("*.md"):
+                if not str(po) in exFiles:
+                    files.append(str(po))
 
-for file in files:
-    os.system("python ProofLeader/converter.py " + file)
-    print(file + " : \033[32mOK\033[0m")
+    root = "./"+dir
+    recursive(pathlib.Path(root))
 
-print("converter : \033[32mALL OK\033[0m")
+    for file in files:
+        converter.converter(file)
+        print(file + " : \033[32mOK\033[0m")
+
+    print("converter : \033[32mALL OK\033[0m")
