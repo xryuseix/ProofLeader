57a42020413477a55e29d150b3c59a377d052ef8
xryuseix <ryusei143.shootingstar@gmail.com>
2020-07-09 09:43:34 +0900

    二度校正すると余分な空白が挿入されるバグを修正

--- ProofLeader/converter.py
+++ ProofLeader/converter.py
@@ -30,11 +30,13 @@ def word2Word(text, file):
 
 # 数字を三桁ごとに区切ってカンマ
 def comma(num):
-    s = num.split('.')
+    beforeCommaNum = num.count(",")
+    s = num.split(".")
     ret = re.sub("(\d)(?=(\d\d\d)+(?!\d))", r"\1,", s[0])
-    if (len(s) > 1):
-        ret += '.' + s[1]
-    return ret
+    if len(s) > 1:
+        ret += "." + s[1]
+    return ret, ret.count(',') - beforeCommaNum
+
 
 # 前後に空白を入れる
 def space(text):
@@ -46,13 +48,27 @@ def space(text):
     for i in range(len(delIndex) - 1):
         subText = text[delIndex[i][1] : delIndex[i + 1][0]]
         if i % 2 == 0:
-            subText = re.sub("([^\n\d, ])([+-]?(?:\d+\.?\d*|\.\d+))", r"\1 \2", subText)  # 数値の前に空白
-            subText = re.sub("([+-]?(?:\d+\.?\d*|\.\d+))([^\n\d, ])", r"\1 \2", subText)  # 数値の後ろに空白
-            subText = re.sub("(\n[a-zA-Z]+)([亜-熙ぁ-んァ-ヶ])", r"\1 \2", subText)  # 先頭英字の後ろに空白
-            
+            subText = re.sub(
+                "([^\n\d, \.])([+-]?(?:\d+\.?\d*|\.\d+))", r"\1 \2", subText
+            )  # 数値の前に空白
+            subText = re.sub(
+                "([+-]?(?:\d+\.?\d*|\.\d+))([^\n\d, \.])", r"\1 \2", subText
+            )  # 数値の後ろに空白
+            subText = re.sub(
+                "(\n[a-zA-Z]+)([亜-熙ぁ-んァ-ヶ])", r"\1 \2", subText
+            )  # 先頭英字の後ろに空白
+
             numPoses = re.finditer("([+-]?(?:\d+\.?\d*|\.\d+))", subText)
-            for p in numPoses: # 三桁ごとにカンマ
-                subText = subText[0 : p.span()[0]] + comma(subText[p.span()[0]:p.span()[1]]) + subText[p.span()[1]:]
+            shift = 0  # カンマを置いた回数
+            for p in numPoses:  # 三桁ごとにカンマ
+                # print(p.span())
+                s, tmpShift = comma(subText[p.span()[0] + shift : p.span()[1] + shift])
+                subText = (
+                    subText[0 : p.span()[0] + shift]
+                    + s
+                    + subText[p.span()[1] + shift :]
+                )
+                shift += tmpShift
             if i + 1 < len(delIndex):
                 resText += subText + text[delIndex[i + 1][0] : delIndex[i + 1][1]]
             else:
