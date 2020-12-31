1aa46ae13e1266f23254d2253d2a0cc900db1d10
xryuseix <ryusei143.shootingstar@gmail.com>
2021-01-01 02:07:57 +0900

    split text

--- ProofLeader/proofLeader.py
+++ ProofLeader/proofLeader.py
@@ -19,15 +19,14 @@ import readme_dfs
 from docopt import docopt
 
 args = docopt(__doc__)
+root = sys.argv[0][:-14] ## ProofLeaderのルートパス
 
 if args['--version']:
     with open('ProofLeader/.version') as f:
         print(f.read())
+    exit()
 
-if args['--file']:
-    readme_dfs.dfs(args['--file'][0], search = args['--search'])
-else:
-    readme_dfs.dfs(search = args['--search'])
+readme_dfs.dfs(dir=args['--file'][0], search = args['--search'])
     
 
 print("\033[32mCHECK!!\033[0m -> https://competent-morse-3888be.netlify.app/")
