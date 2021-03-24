621cb6ad21c2840e00e955bdde5329b3412dd2f0
xryuseix <ryusei143.shootingstar@gmail.com>
2021-03-25 00:46:29 +0900

    test: コメントアウトを入れた

--- test/test.py
+++ test/test.py
@@ -25,6 +25,7 @@ def read_file(path):
         return f.read()
 
 
+# ファイルは直接上書きするので，コピーしておく
 os.system("sudo rm -rf ./test/result")
 os.system("sudo cp -r ./test/testcase ./test/result")
 os.system("sudo python3 ./ProofLeader/proofLeader.py --file ./test/result")
@@ -32,9 +33,10 @@ print("\n" + "-" * 30 + "\n")
 
 path = get_file_names("./test/result")
 
-same_count = 0
-failed_list = []
+same_count = 0  # 正解数
+failed_list = []  # 失敗リスト
 
+# ProofLeaderにファイル探索機能があるけど，カウントとかしたいので一つ一つチェックする
 for i, result_path in enumerate(path):
     testcase_path = result_path.replace("result", "ans")
     testcase_doc = read_file(testcase_path)
@@ -44,13 +46,14 @@ for i, result_path in enumerate(path):
     else:
         failed_list.append(testcase_path)
 
-
+# 正誤チェック
 if same_count == len(path):
     print("\033[32m[PASSED]\033[0m ", end="")
 else:
     print("\033[31m[FAILED]\033[0m ", end="")
 print("test : ({} / {}) passed!".format(same_count, len(path)))
 
+# 失敗したファイルがある時，失敗リストを出力
 if same_count != len(path):
     for f in failed_list:
         print("\033[33mWARNING\033[0m : {}".format(f))
